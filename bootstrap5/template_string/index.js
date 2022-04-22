5cde754887b3a96dfae6733d108be07a611730f8
NataliRodriguez <brendulis.2005@gmail.comm>
2022-04-22 14:24:36 -0300

    template string

---
+++ bootstrap5/template_string/index.js
@@ -0,0 +1,18 @@
+function mostrar(){
+
+    const nombre_apellido = 'Brenda Natali Rodriguez'
+    console.log('nombre y apellido:'+nombre_apellido)
+
+}
+
+function mostrar2(){
+
+    const nombre_apellido= 'Brenda Rodriguez'
+
+    //template string-backticks de Javascript
+    console.log(`
+       nombre y apellido:${nombre_apellido}
+       DNI: 45789152
+       correo: brendulis.2005@gmail.com
+    `)
+}
